681aa549e78e36760cf840f97f0e697768d63651
simao2silva <simao.silva@ua.pt>
2021-01-18 10:28:06 +0000

    2

--- simao.js
+++ simao.js
@@ -5,21 +5,20 @@ function aparecer(x) {
            document.getElementsByClassName("a0")[0].style.display = "block";
            document.getElementsByClassName("a2")[0].style.display = "none";
            document.getElementsByClassName("a3")[0].style.display = "none";
-           document.getElementsByClassName("a5")[0].style.backgroundImage = "url('none')";
                    break;
                case 3:
                document.getElementsByClassName("a1")[0].style.display = "none";
            document.getElementsByClassName("a0")[0].style.display = "none";
            document.getElementsByClassName("a2")[0].style.display = "block";
            document.getElementsByClassName("a3")[0].style.display = "none";
-           document.getElementsByClassName("a5")[0].style.backgroundImage = "url('none')";
+           
                    break;
                case 4: 
                document.getElementsByClassName("a1")[0].style.display = "none";
            document.getElementsByClassName("a0")[0].style.display = "none";
            document.getElementsByClassName("a2")[0].style.display = "none";
            document.getElementsByClassName("a3")[0].style.display = "block";
-           document.getElementsByClassName("a5")[0].style.backgroundImage = "url('none')";
+          
                    break;
            
                default:
